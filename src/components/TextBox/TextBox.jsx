2ced0f0456cf05b1c9679e5b25cec6e5b49f7cd0
codingwolf-at <atameshwari9@gmail.com>
2022-03-03 13:00:57 +0530

    refac: removed redundant element

--- src/components/TextBox/TextBox.jsx
+++ src/components/TextBox/TextBox.jsx
@@ -20,13 +20,11 @@ export const TextBox = () => {
   }
 
   return (
-    <Container size="md">
       <InputGroup>
         <Input onChange={(e) => inputHandler(e)} value={inputValue} borderRightRadius={0} variant="filled" placeholder="Enter message" size="lg" />
           <Button onClick={buttonHandler} borderLeftRadius={0} border="none" bg="gray.100" variant="outline" size='lg' >
             Send
           </Button>
       </InputGroup>
-    </Container>
   )
 }
