539c6fafb839d2e481af4f2e1a82e686b72c017a
codingwolf-at <atameshwari9@gmail.com>
2022-03-03 14:05:32 +0530

    feat: localStorage implemented

--- src/contexts/chat-context.js
+++ src/contexts/chat-context.js
@@ -1,9 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
   const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    const chatData = JSON.parse(localStorage.getItem('chat'));
+    if (chatData.length > 0) {
+      setChat(chatData)
+    } 
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('chat', JSON.stringify(chat));
+  }, [chat])
+
   return (
     <ChatContext.Provider value={{ chat, setChat }} >
       { children }
