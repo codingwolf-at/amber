905861063549dbac632e4c4e07dc5709156cd506
codingwolf-at <atameshwari9@gmail.com>
2022-03-03 10:53:51 +0530

    style: added container element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 import { useContext } from "react";
 import { Header } from "./components";
 import { ChatContext } from "./contexts";
@@ -8,9 +8,9 @@ export default function App() {
   const { chat } = useContext(ChatContext);
   console.log(chat)
   return (
-    <div className="App">
+    <Container maxWidth="md">
       <Header name="Amber" />
       <h1>Hello CodeSandbox</h1>
-    </div>
+    </Container>
   );
 }
