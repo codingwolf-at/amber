9d2537585c2428ada0cab33e0c7a66042be849a6
codingwolf-at <atameshwari9@gmail.com>
2022-03-03 12:47:24 +0530

    feat: input element working & updating chat state

--- src/components/TextBox/TextBox.jsx
+++ src/components/TextBox/TextBox.jsx
@@ -1,11 +1,29 @@
-import { Button, Container, Input, InputGroup, InputRightElement } from "@chakra-ui/react"
+import { v4 as uuidv4 } from 'uuid';
+import { Button, Container, Input, InputGroup } from "@chakra-ui/react"
+import { useContext, useState } from "react"
+import { ChatContext } from "../../contexts";
 
 export const TextBox = () => {
+  const [inputValue, setInputValue] = useState("");
+  const { chat, setChat } = useContext(ChatContext);
+  
+  const inputHandler = (e) => {
+    setInputValue(e.target.value)
+  };
+  
+  const buttonHandler = () => {
+    setChat(value => value.concat({
+      id: uuidv4(),
+      text: inputValue,
+      author: "user"
+    }));
+  }
+
   return (
     <Container size="md">
       <InputGroup>
-        <Input borderRightRadius={0} variant="filled" placeholder="Enter message" size="lg" />
-          <Button borderLeftRadius={0} border="none" bg="gray.100" variant="outline" size='lg' >
+        <Input onChange={(e) => inputHandler(e)} value={inputValue} borderRightRadius={0} variant="filled" placeholder="Enter message" size="lg" />
+          <Button onClick={buttonHandler} borderLeftRadius={0} border="none" bg="gray.100" variant="outline" size='lg' >
             Send
           </Button>
       </InputGroup>
